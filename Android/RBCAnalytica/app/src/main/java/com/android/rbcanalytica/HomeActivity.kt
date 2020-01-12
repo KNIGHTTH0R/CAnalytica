f270baeb257b63e0203afd58f5d392de5dbd3ac8
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2020-01-12 03:06:51 -0500

    Added home screen

--- Android/RBCAnalytica/app/src/main/java/com/android/rbcanalytica/HomeActivity.kt
+++ Android/RBCAnalytica/app/src/main/java/com/android/rbcanalytica/HomeActivity.kt
@@ -1,12 +1,32 @@
 package com.android.rbcanalytica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        facebookButton.setOnClickListener { navigateToFacebookScreen() }
+        twitterButton.setOnClickListener { navigateToTwitterScreen() }
+        redditButton.setOnClickListener { navigateToRedditScreen() }
+    }
+
+    private fun navigateToRedditScreen() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToTwitterScreen() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToFacebookScreen() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
